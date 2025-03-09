253443815c51120e6cc51e8674f854e831aba708
Vikalp639 <vikalpchauhan412@gmail.com>
2025-03-09 05:30:09 +0530

    gsg

--- client/src/Page/Home.js
+++ client/src/Page/Home.js
@@ -186,7 +186,7 @@ const getproducts=async()=>{
                    
                    <div className='flex gap-[25px] flex-wrap w-[1200px]   overflow-y-hidden mt-[20px] p-[20px]'>
                    {
-                   products.map((p)=>(
+                   products?.map((p)=>(
 <div  key={p._id} >
 <img className='w-[200px] h-[160px] rounded-[5%] border-b-[2px] mb-[15px] ml-[15px]' src={`https://e-commy-1.onrender.com//api/v1/getphoto/${p._id}`} alt='photoff'onClick={()=>{
                         navi(`/singleproduct/${p.slug}`);
